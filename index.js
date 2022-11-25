420cf73c23d28e3e1a244a8a9aa541cad82b1d1e
Armanul144894 <armanul15-2842@diu.edu.bd>
2022-11-25 16:56:02 +0600

    add dotenv

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://<username>:<password>@cluster0.g6tcnhj.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 app.get("/", (req, res) => {
   res.send("deal point server running");
 });
